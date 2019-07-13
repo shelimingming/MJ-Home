b98416bdcf8c37a2e32d90cb962d7c19624fe826
shelimingming <shelimingming@qq.com>
2019-07-13 21:13:53 +0800

    Initial Commit

---
+++ pages/list/list.js
@@ -0,0 +1,86 @@
+// pages/list/list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.type)
+    console.log(options.name)
+
+    const api = "https://api-m.mtime.cn/" + options.type+"?locationId=290"
+    console.log(api)
+
+    const _this = this
+
+    wx.request({
+      url: api, //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success(res) {
+        _this.setData({
+          list: res.data.ms
+        })
+        console.log(res.data)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
